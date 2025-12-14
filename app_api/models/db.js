a7e91b0a15f8315f3de85d00b5871167e0ff848b
FatihSahin0707 <sahin.fatih0704@gmail.com>
2025-12-14 19:17:33 +0300

    Değişiklik

--- app_api/models/db.js
+++ app_api/models/db.js
@@ -1,17 +1,26 @@
-var mongoose=require('mongoose');
-//var dbURI='mongodb://localhost/mekanbul';
-var dbURI = 'mongodb+srv://FatihSahin:fatih123@cluster0.tlrc7d6.mongodb.net/?appName=Cluster0';
+var mongoose = require('mongoose');
+
+var dbURI = 'mongodb://localhost:27017/mekanbul';
+//var dbURI = 'mongodb+srv://FatihSahin:fatih123@cluster0.comwejd.mongodb.net/?appName=Cluster0';
+
 mongoose.connect(dbURI);
-mongoose.connection.on("connected",function (){
-    console.log(dbURI+" adresindeki veritabanına bağlanıldı!\n");
+
+mongoose.connection.on('connected', function () {
+    console.log('Mongoose ' + dbURI + ' adresindeki veritabanına bağlandı\n');
 });
-mongoose.connection.on("disconnected",function(){
-    console.log("Bağlantı kesildi!\n");
+
+mongoose.connection.on('error', function (err) {
+    console.log('Mongoose bağlantı hatası\n: ' + err);
 });
-//Uygulama kapandığında kapat.
-process.on("SIGINT",function(){
+
+mongoose.connection.on('disconnected', function () {
+    console.log('Mongoose bağlantısı kesildi\n');
+});
+
+process.on("SIGINT", function () {
     mongoose.connection.close();
-    console.log("Bağlantı kapatıldı");
+    console.log('Bağlantı kapatıldı');
     process.exit(0);
 });
+
 require("./venue");
--- app_api/models/venue.js
+++ app_api/models/venue.js
@@ -1,30 +1,23 @@
 var mongoose=require("mongoose");
-var venue = new mongoose.Schema({ }); 
-
-var hour=new mongoose.Schema(
-    {
+var hour=new mongoose.Schema({
     day:{type:String,required:true},
-    open:String,
-    close:String,
+    open:{type:String,required:true},
+    close:{type:String,required:true},
     isClosed:{type:Boolean,default:false}
-    }
-);
-
-var comment=new mongoose.Schema(
-    {
-        author:{type:String,required:true},
-        rating:{type:Number,required:true,min:0,max:5,},
-        text:{type:String,required:true},
-        date:{type:Date,default:Date.now}
-    }
-);
+});
+var comment=new mongoose.Schema({
+    author:{type:String,required:true},
+    rating:{type:Number,required:true,min:0,max:5},
+    text:{type:String,required:true},
+    date:{type:Date,default:Date.now}
+});
 var venue=new mongoose.Schema({
     name:{type:String,required:true},
-    address:String,
-    rating:{type:Number,min:0,max:5,default:0},
-    coordinates: {type: [Number], index: '2dsphere' },
-    foodanddrink:[String],
-    hours:[hour],
-    comments:[comment]
-    });
+    address:{type:String,required:true},
+   rating:{type:Number,default:0,min:0,max:5},
+   foodanddrink:[String],
+   coordinates:{type:[Number],index:"2dsphere"},
+   hours:[hour],
+   comments:[comment]
+});
 mongoose.model("venue",venue,"venues");
